09359139bc11b8caa170d5381b40fedd0a561a5e
unknown <lsmxy4@gmail.com>
2026-01-28 11:48:07 +0900

    fixed

--- js/app.js
+++ js/app.js
@@ -1,63 +1,75 @@
+// alert("hello javascript")
+
 const body = document.querySelector("body");
 const header = document.querySelector("header");
 const fixedArrow = document.querySelector(".fixed-arrow");
 const mobNavBtn = document.querySelector(".mob-nav-btn");
 const navList = document.querySelectorAll(".nav-list li");
 
+// [0,1,2,3]
+
 navList.forEach((btn, i) => {
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    document.querySelector(`#sec-${i + 1}`).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
+ btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(document.querySelector(`#sec-${i + 1}`));
+
+
+  document.querySelector(`#sec-${i + 1}`).scrollIntoView({
+    behavior:'smooth'
+  })
+ });
 });
 
+// classList.toggle() 주체 body
+
+// console.log(fixedArrow)
+
 mobNavBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  body.classList.toggle("mob-nav-open");
+ e.preventDefault();
+ body.classList.toggle("mob-nav-open");
 });
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY >= 100) {
-    body.classList.add("scroll");
-  } else {
-    body.classList.remove("scroll");
-  }
+ if (window.scrollY >= 100) {
+  body.classList.add("scroll");
+ } else {
+  body.classList.remove("scroll");
+ }
 });
 
 fixedArrow.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
+ window.scrollTo({
+  top: 0,
+  behavior: "smooth",
+ });
 });
 
-const section = document.querySelector('.sec-5');
-const contents = document.querySelectorAll('.sec-5 .content');
-const menuLinks = document.querySelectorAll('.sec-5 .menu li a');
 
-const bgImages = [
-  'Frame0.png',
-  'Frame1.png',
-  'Frame2.png',
-  'Frame3.png'
-];
+const sec5MenuList = document.querySelectorAll(".sec-5-menu li");
+const sec5ContentList = document.querySelectorAll(".sec-5-content-wrap .content");
+let crr = 0;
 
-contents[0].classList.add('active');
-menuLinks[0].classList.add('active');
-section.style.backgroundImage = `url(../img/${bgImages[0]})`;
 
-menuLinks.forEach((link, index) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
 
-    contents.forEach(c => c.classList.remove('active'));
-    menuLinks.forEach(m => m.classList.remove('active'));
+init(crr);
 
-    contents[index].classList.add('active');
-    link.classList.add('active');
+function init(x) {
+ sec5MenuList[x].classList.add("active");
+ sec5ContentList[x].classList.add("on");
+}
 
-    section.style.backgroundImage = `url(../img/${bgImages[index]})`;
-  });
+function reset() {
+ for (let i = 0; i < sec5MenuList.length; i++) {
+  sec5MenuList[i].classList.remove("active");
+  sec5ContentList[i].classList.remove("on");
+ }
+}
+
+sec5MenuList.forEach((menu, index) => {
+ menu.addEventListener("click", (e) => {
+    e.preventDefault()
+  // console.log(menu,index)
+  reset();
+  init(index);
+ });
 });
